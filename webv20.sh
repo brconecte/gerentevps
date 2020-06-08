383c5049140cd080e3659422868c01a3a9626832
brconecte <65625172+brconecte@users.noreply.github.com>
2020-06-08 12:51:08 -0300

    Update webv20.sh

--- webv20.sh
+++ webv20.sh
@@ -1,21 +1,21 @@
-#!/bin/bash
-clear
-echo "America/Sao_Paulo" > /etc/timezone
-ln -fs /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
-rm *.sh > /dev/null 2>&1
-link='sshvip.ducksdns.org'
-wget $link/usr/scripts/criarusuario.sh > /dev/null 2>&1
-wget $link/usr/scripts/remover.sh > /dev/null 2>&1
-wget $link/usr/scripts/AlterarSenha.sh > /dev/null 2>&1
-wget $link/usr/scripts/AlterarData.sh > /dev/null 2>&1
-wget $link/usr/scripts/KillUser.sh > /dev/null 2>&1
-wget $link/usr/scripts/sshmonitor.sh > /dev/null 2>&1
-wget $link/usr/scripts/alterarlimite.sh > /dev/null 2>&1
-chmod 777 *sh
-service ssh restart > /dev/null 2>&1
-echo -e "\n\033[1;32mCONCLUIDO!\033[0m"
-sleep 3
-cat /dev/null > ~/.bash_history && history -c && clear
-echo -e "\033[1;36mENTRE NO PAINEL E CRIE UMA CONTA SSH PRA VER SE ESTA TUDO OK!\033[0m"
+#! / bin / bash
+Claro
+eco  " America / Sao_Paulo "  > / etc / fuso horário
+ln -fs / usr / share / zoneinfo / America / Sao_Paulo / etc / localtime
+dpkg-reconfigure - frontend tzdata não interativo
+rm * .sh > / dev / null 2> & 1
+link = ' https://github.com/brconecte/gerentevps/blob/master '
+wget $ link /criarusuario.sh > / dev / null 2> & 1
+wget $ link /remover.sh > / dev / null 2> & 1
+wget $ link /AlterarSenha.sh > / dev / null 2> & 1
+wget $ link /AlterarData.sh > / dev / null 2> & 1
+wget $ link /KillUser.sh > / dev / null 2> & 1
+wget $ link /sshmonitor.sh > / dev / null 2> & 1
+wget $ link /alterarlimite.sh > / dev / null 2> & 1
+chmod 777 * sh
+serviço ssh restart > / dev / null 2> & 1
+eco -e " \ n \ 033 [1; 32mCONCLUIDO! \ 033 [0m "
+dormir 3
+cat / dev / null >  ~ /.bash_history &&  history -c && clear
+echo -e " \ 033 [1; 36MENTRE NO PAINEL E CRIE UMA CONTA SSH PARA VER SE ESTA TUDO OK! \ 033 [0m "
 rm inst
